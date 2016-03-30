4f73d499e29a538a1675032c6fe50191a1ca59cf
brennmat <brennmat@gmail.com>
2016-03-30 13:21:20 +0200

    Added licence (GPL)

--- python/classes/rgams.py
+++ python/classes/rgams.py
@@ -1,4 +1,39 @@
 # Code for the SRS RGA mass spec class
+# 
+#
+# Copyright 2016, Matthias Brennwald (brennmat@gmail.com) and Yama Tomonaga
+# 
+# This file is part of ruediPy, a toolbox for operation of RUEDI mass spectrometer systems.
+# 
+# ruediPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ruediPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ruediPy.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# ruediPy: toolbox for operation of RUEDI mass spectrometer systems
+# Copyright (C) 2016  Matthias Brennwald
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import serial
 import time
@@ -84,7 +119,7 @@ class rgams:
 	
 		# check if serial buffer (input) is empty (just in case, will be useful to catch errors):
 		if self.ser.inWaiting() > 0:
-			self.warning('**** DEBUGGING INFO: serial buffer not empty before executing command = ' + cmd + ' ans = ' + ans)
+			self.warning('**** DEBUGGING INFO: serial buffer not empty before executing command = ' + cmd + '.')
 
 		# send command to serial port:
 		self.ser.write(cmd + '\r\n')
--- python/classes/selectorvalve.py
+++ python/classes/selectorvalve.py
@@ -1,4 +1,38 @@
 # Code for the VICI selector valve class
+# 
+#
+# Copyright 2016, Matthias Brennwald (brennmat@gmail.com) and Yama Tomonaga
+# 
+# This file is part of ruediPy, a toolbox for operation of RUEDI mass spectrometer systems.
+# 
+# ruediPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ruediPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ruediPy.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# ruediPy: toolbox for operation of RUEDI mass spectrometer systems
+# Copyright (C) 2016  Matthias Brennwald
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import serial
 import time
